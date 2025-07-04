38cc784c6aa8425a884edb0f26a4429389f13d43
GBGodea <125504532+GBGodea@users.noreply.github.com>
2025-07-04 22:17:40 +0400

    refactored init-data

--- init-data.sql
+++ init-data.sql
@@ -1,5 +1,28 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
+CREATE TABLE IF NOT EXISTS roles (
+    id   UUID PRIMARY KEY,
+    role VARCHAR(50) NOT NULL UNIQUE
+    );
+
+CREATE TABLE IF NOT EXISTS users (
+    id       UUID PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_id  UUID NOT NULL REFERENCES roles(id)
+    );
+
+CREATE TABLE IF NOT EXISTS files (
+    id               UUID PRIMARY KEY,
+    original_name    VARCHAR(255) NOT NULL,
+    stored_name      VARCHAR(255) NOT NULL UNIQUE,
+    content_type     VARCHAR(255),
+    size             BIGINT,
+    upload_date      TIMESTAMP NOT NULL,
+    expiration_date  TIMESTAMP NOT NULL,
+    download_count   BIGINT NOT NULL DEFAULT 0
+    );
+
 INSERT INTO roles(id, role)
 SELECT gen_random_uuid(),
        'user' WHERE NOT EXISTS(
